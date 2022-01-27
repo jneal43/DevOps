ffdfd023b629f96842d4081e6d6a2b3e70cc4178
jneal43 <nea17003@byui.edu>
2022-01-27 10:44:01 -0700

    formatting error

--- test/login.test.js
+++ test/login.test.js
@@ -3,7 +3,7 @@ import login from '../utils/login.mjs';
 
 it("Login API should return a valid login token",async ()=>{
 
-    const loginToken= await login({userName:"jared.neal43@gmail.com", password:"Abc32!"});
+    const loginToken = await login({userName:"jared.neal43@gmail.com", password:"Abc32!"});
     assert.equal(loginToken.length,36);
 
 });
